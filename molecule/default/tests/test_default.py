e4005e4212440229b7af04529c2d2f85655c647e
darkwizard242 <am900820@gmail.com>
2020-11-29 17:42:00 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,18 +6,33 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = 'openrazer-meta'
+PACKAGE_BINARY = '/usr/bin/openrazer-daemon'
+
+
 def test_openrazer_package_installed(host):
-    assert host.package("openrazer-meta").is_installed
+    """
+    Tests if openrazer is installed
+    """
+    assert host.package(PACKAGE).is_installed
 
 
 def test_openrazer_binary_exists(host):
-    assert host.file('/usr/bin/openrazer-daemon').exists
+    """
+    Tests if openrazer binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_openrazer_binary_file(host):
-    assert host.file('/usr/bin/openrazer-daemon').is_file
+    """
+    Tests if openrazer binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_openrazer_binary_which(host):
-    assert host.check_output('which openrazer-daemon') == \
-      '/usr/bin/openrazer-daemon'
+    """
+    Tests the output to confirm openrazer's binary location.
+    """
+    assert host.check_output('which openrazer-daemon') == PACKAGE_BINARY
